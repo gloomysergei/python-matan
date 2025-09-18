4085b2eac5df7c5533af08eb559601ae8567d7ab
gloomysergei <almazopal@gmail.com>
2025-09-18 09:46:37 +0300

    feat: installation Numpy, matplotlab usinguv add ...

---
+++ hexlet_python_package/template.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print(arr[2])
+
+using = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+element = using[1, 2]
+print(element)
+print(arr[1:4])
